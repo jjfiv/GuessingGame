68e301257e83b233b446f8443160946e88053c13
John Foley <jjfoley@smith.edu>
2019-01-21 11:37:55 -0500

    Update GuessingGame.java

--- src/main/java/GuessingGame.java
+++ src/main/java/GuessingGame.java
@@ -1,4 +1,5 @@
 public class GuessingGame {
+    // I mean for you to delete this code or start typing over it.
     public static void main(String[] args) {
         System.out.println("Just Hello, World! for now.");
     }
